a310a56f97872671fa1a992469b01dff5ab675b2
mahmoud azeezeh <mahmmoadaziza@gmail.com>
2025-03-05 08:41:58 +0200

    vite.config.ts updeut

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Digital-web/',    // <-- ضع اسم مستودعك هنا مع سلاش في البداية والنهاية
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
